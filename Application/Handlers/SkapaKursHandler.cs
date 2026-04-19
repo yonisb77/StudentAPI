1f2ce741582e8c3b954000efd56a86e8a8392f00
yonisb77 <yonis_77@hotmail.com>
2026-04-19 18:44:13 +0200

    Lagt till validering i SkapaKursHandler för att förhindra tomma kursnamn

--- Application/Handlers/SkapaKursHandler.cs
+++ Application/Handlers/SkapaKursHandler.cs
@@ -3,23 +3,37 @@ using Domain.Entities;
 using Domain.Interfaces;
 using MediatR;
 
-namespace Application.Handlers;
-
-public class SkapaKursHandler : IRequestHandler<SkapaKursCommand, int>
+namespace Application.Handlers
 {
-    private readonly IKursRepository _repository;
-    public SkapaKursHandler(IKursRepository repository) => _repository = repository;
-
-    public async Task<int> Handle(SkapaKursCommand request, CancellationToken ct)
+    public class SkapaKursHandler : IRequestHandler<SkapaKursCommand, int>
     {
-        var nyKurs = new Kurs
-        {
-            Kursnamn = request.Kursnamn,
-            Poang = request.Poang,
-            LarareId = request.LarareId
-        };
+        private readonly IKursRepository _repository;
 
-        await _repository.SkapaAsync(nyKurs);
-        return nyKurs.Id;
+        public SkapaKursHandler(IKursRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<int> Handle(SkapaKursCommand request, CancellationToken ct)
+        {
+            // Validering (Commit 4)
+            if (string.IsNullOrWhiteSpace(request.Kursnamn))
+            {
+                throw new ArgumentException("Kursnamn får inte vara tomt.");
+            }
+
+            // Loggning (Commit 5)
+            Console.WriteLine($"[LOG]: Skapar kurs: {request.Kursnamn}");
+
+            var nyKurs = new Kurs
+            {
+                Kursnamn = request.Kursnamn,
+                Poang = request.Poang,
+                LarareId = request.LarareId
+            };
+
+            await _repository.SkapaAsync(nyKurs);
+            return nyKurs.Id;
+        }
     }
 }
