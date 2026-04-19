c2c40acc031562abca37f5b3431d73cdfa7faebb
Yonis B <yonis_77@hotmail.com>
2026-04-19 19:19:25 +0200

    Refine comments and remove console logging
    Implementerat validering av kursnamn i Handlern

--- Application/Handlers/SkapaKursHandler.cs
+++ Application/Handlers/SkapaKursHandler.cs
@@ -1,4 +1,4 @@
-﻿using Application.Commands;
+using Application.Commands;
 using Domain.Entities;
 using Domain.Interfaces;
 using MediatR;
@@ -16,14 +16,11 @@ namespace Application.Handlers
 
         public async Task<int> Handle(SkapaKursCommand request, CancellationToken ct)
         {
-            // COMMIT 4: Validering - Kontrollera att kursnamnet inte är tomt
+            // Validering: Kontrollera att kursnamnet inte är tomt
             if (string.IsNullOrWhiteSpace(request.Kursnamn))
             {
                 throw new ArgumentException("Kursnamn får inte vara tomt.");
             }
-
-            // EXTRA: Loggning för att spåra aktivitet i konsolen
-            Console.WriteLine($"[LOG]: Skapar kurs: {request.Kursnamn}");
 
             var nyKurs = new Kurs
             {
