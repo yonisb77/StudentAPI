7b936468fc01ec58683e330647f8cdbfbfbc5182
yonisb77 <yonis_77@hotmail.com>
2026-04-19 19:09:14 +0200

    Grundstruktur för SkolsystemAPI

--- Application/Handlers/SkapaKursHandler.cs
+++ Application/Handlers/SkapaKursHandler.cs
@@ -3,23 +3,37 @@ using Domain.Entities;
 using Domain.Interfaces;
 using MediatR;
 
-namespace Application.Handlers;
-
-public class SkapaKursHandler : IRequestHandler<SkapaKursCommand, int>
+namespace Application.Handlers
 {
-    private readonly IKursRepository _repository;
-    public SkapaKursHandler(IKursRepository repository) => _repository = repository;
-
-    public async Task<int> Handle(SkapaKursCommand request, CancellationToken ct)
+    public class SkapaKursHandler : IRequestHandler<SkapaKursCommand, int>
     {
-        var nyKurs = new Kurs
-        {
-            Kursnamn = request.Kursnamn,
-            Poang = request.Poang,
-            LarareId = request.LarareId
-        };
+        private readonly IKursRepository _repository;
 
-        await _repository.SkapaAsync(nyKurs);
-        return nyKurs.Id;
+        public SkapaKursHandler(IKursRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<int> Handle(SkapaKursCommand request, CancellationToken ct)
+        {
+            // COMMIT 4: Validering - Kontrollera att kursnamnet inte är tomt
+            if (string.IsNullOrWhiteSpace(request.Kursnamn))
+            {
+                throw new ArgumentException("Kursnamn får inte vara tomt.");
+            }
+
+            // EXTRA: Loggning för att spåra aktivitet i konsolen
+            Console.WriteLine($"[LOG]: Skapar kurs: {request.Kursnamn}");
+
+            var nyKurs = new Kurs
+            {
+                Kursnamn = request.Kursnamn,
+                Poang = request.Poang,
+                LarareId = request.LarareId
+            };
+
+            await _repository.SkapaAsync(nyKurs);
+            return nyKurs.Id;
+        }
     }
 }
