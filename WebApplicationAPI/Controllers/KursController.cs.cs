5e4eeebc38d9ef3dc19ee21e2c5cea92653bff58
yonisb77 <yonis_77@hotmail.com>
2026-04-23 19:00:09 +0200

    uppdate

--- WebApplicationAPI/Controllers/KursController.cs.cs
+++ WebApplicationAPI/Controllers/KursController.cs.cs
@@ -27,36 +27,32 @@ namespace API.Controllers
         public async Task<IActionResult> Skapa(SkapaKursCommand command)
         {
             var kursId = await _mediator.Send(command);
-            
-            // Returnerar 201 Created istället för bara OK
             return CreatedAtAction(nameof(Skapa), new { id = kursId }, "Kursen har skapats framgångsrikt.");
         }
-    }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> HamtaViaId(int id)
+        {
+            var kurs = await _mediator.Send(new HamtaKursViaIdQuery(id));
+            if (kurs == null) return NotFound();
+            return Ok(kurs);
+        }
 
-    [HttpGet("{id}")]
-    public async Task<IActionResult> HamtaViaId(int id)
-    {
-        var kurs = await _mediator.Send(new HamtaKursViaIdQuery(id));
-        if (kurs == null) return NotFound();
-        return Ok(kurs);
-    }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Uppdatera(int id, UppdateraKursCommand command)
+        {
+            if (id != command.Id) return BadRequest("Id matchar inte.");
+            var result = await _mediator.Send(command);
+            if (!result) return NotFound();
+            return Ok("Kurs uppdaterad.");
+        }
 
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Uppdatera(int id, UppdateraKursCommand command)
-    {
-        if (id != command.Id) return BadRequest("Id matchar inte.");
-        var result = await _mediator.Send(command);
-        if (!result) return NotFound();
-        return Ok("Kurs uppdaterad.");
-    }
-
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> Radera(int id)
-    {
-        var result = await _mediator.Send(new RaderaKursCommand(id));
-        if (!result) return NotFound();
-        return Ok("Kurs raderad.");
-    }
-
-}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Radera(int id)
+        {
+            var result = await _mediator.Send(new RaderaKursCommand(id));
+            if (!result) return NotFound();
+            return Ok("Kurs raderad.");
+        }
+    } // Slut på klassen
+} // Slut på namespacet
